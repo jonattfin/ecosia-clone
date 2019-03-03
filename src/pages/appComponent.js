4c2b8272e3a6e16cc3432603dcc0be7c9789b106
Catalin Andrei <jonattfin@gmail.com>
2019-03-03 18:09:12 +0200

    wip

--- src/pages/appComponent.js
+++ src/pages/appComponent.js
@@ -2,23 +2,36 @@ import React, { Suspense, lazy } from 'react';
 import {
   BrowserRouter as Router,
   Route,
-  Switch
+  Switch,
 } from 'react-router-dom'
 import { Badge } from 'reactstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBell, faBars } from '@fortawesome/free-solid-svg-icons'
+import { faTree, faBars } from '@fortawesome/free-solid-svg-icons'
 import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
 
 // import logo from './logo.svg';
 import './styles.scss';
 
 const Home = lazy(() => import('./home'));
+const AboutUs = lazy(() => import('./aboutUs'));
+
+const pages = [{
+  url: '/aboutUs', description: 'About us'
+}, {
+  url: '/howItWorks', description: 'How Ecosia works'
+}, {
+  url: '/mobileApp', description: 'Mobile app'
+}];
 
 class CustomDropDown extends React.Component {
   state = { dropdownOpen: false }
 
   toggle = () => {
     this.setState({ dropdownOpen: !this.state.dropdownOpen });
+  }
+
+  onClick = (url, history) => () => {
+    history.push(url);
   }
 
   render() {
@@ -28,12 +41,11 @@ class CustomDropDown extends React.Component {
           <FontAwesomeIcon icon={faBars} color="goldenrod" />
         </DropdownToggle>
         <DropdownMenu>
-          <DropdownItem>How Ecosia works</DropdownItem>
-          <DropdownItem>About us</DropdownItem>
-          <DropdownItem>Mobile app</DropdownItem>
-          <DropdownItem>Privacy</DropdownItem>
-          <DropdownItem divider />
-          <DropdownItem>Settings</DropdownItem>
+          {pages.map(page => (
+            <Route render={({ history }) => (
+              <DropdownItem onClick={this.onClick(page.url, history)}>{page.description}</DropdownItem>
+            )} />
+          ))}
         </DropdownMenu>
       </Dropdown>
     );
@@ -46,7 +58,7 @@ export default (props) => (
       <div className="content">
         <div className="menu">
           <div>
-            <FontAwesomeIcon icon={faBell} color="goldenrod" /> <Badge color="info">{props.model.numberOfSearches}</Badge>
+            <FontAwesomeIcon icon={faTree} color="green" /> <Badge color="info">{props.model.numberOfSearches}</Badge>
           </div>
           <CustomDropDown />
         </div>
@@ -55,6 +67,7 @@ export default (props) => (
       <Suspense fallback={<div>Loading...</div>}>
         <Switch>
           <Route exact path="/" component={Home} />
+          <Route exact path="/aboutUs" component={AboutUs} />
         </Switch>
       </Suspense>
     </div>
