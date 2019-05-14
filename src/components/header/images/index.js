04583b82624d1c53cf6cd8296cce662ab9710a6c
jonattfin <jonattfin@gmail.com>
2019-05-14 14:16:06 +0300

    wip

---
+++ src/components/header/images/index.js
@@ -0,0 +1,5 @@
+import { default as ecosiaLogo } from './ecosia.svg';
+
+export default {
+  ecosiaLogo,
+}
--- src/components/header/index.js
+++ src/components/header/index.js
@@ -11,23 +11,31 @@ import {
   Route,
 } from 'react-router-dom'
 
+import images from './images';
 import styles from './styles.module.scss';
 
 const menu = createMenu();
 
-export default ({ model = {} }) => (
-  <div className={styles.header}>
-    <Tag className={styles.tree} icon={'tree'} round>
-      {model.payload.numberOfSearches}
-    </Tag>
-    <Popover content={menu} position={Position.BOTTOM_LEFT}>
-      <Button icon="menu" />
-    </Popover>
-  </div>
-);
+export default ({ model = ({ payload: {} }), }) => {
+  let showTrees = typeof window !== undefined && window.location.pathname === '/';
+
+  return (
+    <div className={styles.header}>
+      {showTrees && (
+        <Tag className={styles.tree} icon={'tree'} round>
+          {model.payload.numberOfSearches}
+        </Tag>
+      )}
+      <Popover content={menu} position={Position.BOTTOM_LEFT}>
+        <Button icon="menu" />
+      </Popover>
+    </div>
+  );
+};
 
 function createMenu() {
   const pages = [
+    { url: '/', text: 'Home', icon: "home", },
     { url: '/how-it-works', text: 'How Ecosia works', icon: "build", },
     { url: '/aboutUs', text: 'About us', icon: 'help' },
     { url: '/mobile', text: 'Mobile app', icon: 'mobile-phone' },
