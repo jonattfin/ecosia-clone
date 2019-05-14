523982ca16e828fca94501db8945719c88b9ef48
jonattfin <jonattfin@gmail.com>
2019-05-14 11:03:13 +0300

    wip

---
+++ src/pages/howItWorks/images/index.js
@@ -0,0 +1,21 @@
+import backgroundImage from './background.jpg';
+import upWorthyImage from './up-worthy.svg';
+import scientificImage from './scientific.svg';
+import guardianImage from './guardian.svg';
+import salonImage from './salon.svg';
+import forbesImage from './forbes.svg';
+import ecosiaImage from './ecosia.svg';
+import adsImage from './ads.svg';
+import incomeImage from './income.svg';
+
+export default {
+  backgroundImage,
+  upWorthyImage,
+  scientificImage,
+  guardianImage,
+  salonImage,
+  forbesImage,
+  ecosiaImage,
+  adsImage,
+  incomeImage
+}
--- src/pages/howItWorks/index.js
+++ src/pages/howItWorks/index.js
@@ -1,12 +1,67 @@
 import React from 'react';
+import { Button, Intent } from "@blueprintjs/core";
+import { IconNames } from "@blueprintjs/icons";
 
 import styles from './styles.module.scss';
+import images from './images';
 
 export default () => {
-
   return (
     <div className={styles.how_it_works}>
-      how it works
-    </div>
+      <div className={styles.first_screen}>
+        <div className={styles.title}>Plant trees while you search the web</div>
+        <div className={styles.separator}>&nbsp;</div>
+        <div className={styles.sub_title}>We use the profit we make from your searches to plant trees where they are needed most. <br />Get the free browser extension and plant trees with every search.</div>
+        <div className={styles.separator}>&nbsp;</div>
+        <div>
+          <Button icon={IconNames.ADD} intent={Intent.PRIMARY} large text={'Add Ecosia to Firefox'} />
+        </div>
+      </div>
+      <div className={styles.brands}>
+        {getBrands().map((brand, index) => (<img key={`brand_image_${index}`} className={styles.image} src={brand} alt='tdlr'></img>))}
+      </div>
+      <div className={styles.how_it_works_screen}>
+        <div className={styles.header}>
+          <div className={styles.title}>How it works</div>
+        </div>
+        <div className={styles.images}>
+          {getImages().map((item, index) => (
+            <div key={`image_${index}`}>
+              <img className={styles.image} src={item.image} alt='tdlr'></img>
+              <div className={styles.text}>{item.text}</div>
+            </div>
+          ))}
+        </div>
+
+      </div>
+      <div className={styles.graphics}>
+        <div className={styles.header}>
+          <div className={styles.title}>Over 57 million trees planted</div>
+        </div>
+        <div className={styles.images}>
+          {getImages().map((item, index) => (
+            <div key={`image_${index}`}>
+              <img className={styles.image} src={item.image} alt='tdlr'></img>
+              <div className={styles.text}>{item.text}</div>
+            </div>
+          ))}
+        </div>
+
+      </div>
+    </div >
   );
+}
+
+function getBrands() {
+  const { upWorthyImage, scientificImage, guardianImage, salonImage, forbesImage } = images;
+  return [upWorthyImage, scientificImage, guardianImage, salonImage, forbesImage];
+}
+
+function getImages() {
+  const { ecosiaImage, adsImage, incomeImage } = images;
+  return [
+    { image: ecosiaImage, text: 'You search the web with Ecosia.' },
+    { image: adsImage, text: 'Search ads generate income for Ecosia.' },
+    { image: incomeImage, text: 'Ecosia uses this income to plant trees.' },
+  ];
 }
